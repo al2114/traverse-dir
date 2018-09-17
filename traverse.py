a96090229f906032db3fd09c4a41a0798597ce82
Andrew Li <al2114@ic.ac.uk>
2018-09-17 13:45:00 -0700

    Add plot data function

--- traverse.py
+++ traverse.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from collections import deque
 import re
 
-
+# Takes a path and a pattern and returns the number of
+# files matching the pattern and a list of subdirs
 def search_directory(path, pattern):
     dirs = []
     count = 0
@@ -19,6 +20,8 @@ def search_directory(path, pattern):
     return count, dirs
 
 
+# Takes a path and pattern and returns a dict for each subdir
+# as key and the count of filenames matching as value
 def traverse_directory(root_dir, pattern):
     directory_queue = deque([root_dir])
     res = {}
@@ -30,12 +33,23 @@ def traverse_directory(root_dir, pattern):
             directory_queue.append(os.path.join(path,d))
     return res
 
+def plot_result(res):
+    x = range(len(res))
+    plt.bar(x, list(res.values()), align='center')
+    plt.xticks(x, list(res.keys()))
+    plt.ylabel('files matching keyword')
+    plt.xlabel('subdir pathname')
+    plt.title('Files matching keyword in path')
+    plt.savefig('filecount.png')
+    plt.close()
+
 if __name__ == "__main__":
     if(len(sys.argv) == 3):
         root_dir = sys.argv[1]
         pattern = sys.argv[2]
         res = traverse_directory(root_dir, pattern)
         print res
+        plot_result(res)
 
     else:
         print "Require 2 input arguments: <root_dir> <pattern>"
